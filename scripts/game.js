844d872c1ea1716d4479200ed772c9fb52ab915c
Hector Macafonty <learnwithhector@gmail.com>
2023-01-13 18:56:42 +0000

    check for winner

--- scripts/game.js
+++ scripts/game.js
@@ -30,7 +30,56 @@ function selectGameField(event) {
   selectedField.classList.add("disabled");
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
 
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // checking rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+  // checking columnns
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+  // top left -> bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  // bottom left -> top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  // draw
+  if (currentRound === 9) {
+    return -1;
+  }
+  // no winner but still empty squares
+  return 0;
 }
